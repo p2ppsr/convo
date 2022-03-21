44ec4440d4a82c85c9b427d52e07b2b530909125
bpurtz <40925796+bpurtz@users.noreply.github.com>
2022-03-21 12:40:01 -0700

    bab-155 update prompt (#3)
    * bab-155 update prompt
    
    * bab-155 small fixes
    
    * Use new Babbage React Prompt, update fields
    
    Co-authored-by: Ben Purtzer <purtzerb92@gmail.com>
    Co-authored-by: Ty Everett <ty@tyweb.us>

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import BabbagePrompt from '@babbage/react-prompt'
 import * as serviceWorker from './serviceWorker'
+import Theme from './components/Theme'
 
 ReactDOM.render(
-  <BabbagePrompt appName='Convo Messenger'>
-    <App />
-  </BabbagePrompt>,
+  <Theme>
+    <BabbagePrompt
+      customPrompt
+      appName='Convo Messenger'
+      author='Ty Everett'
+      authorUrl='https://tyeverett.com'
+      description={'Do you value your privacy? We value ours, so we\'ve got your back. Try Convo Messenger, the world\'s first fully encrypted, decentralized messaging app that\'s powered by blockchain technology using Babbage.\nRather than feeding data silos information used to sell you garbage, why not own the messages you share with the people you trust? Own your identity. What a concept  ¯\\_(ツ)_/¯ '}
+      appIcon='/icon.png'
+      appImages={[
+        '/previewSlide.jpeg'
+      ]}
+    >
+      <App />
+    </BabbagePrompt>
+  </Theme>,
   document.getElementById('root')
 )
 
